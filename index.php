47c9d8f7626345cbc38215af9e1725ecf14dbe0c
Mahmut İŞÇİ <mahmutisci@outlook.com>
2016-10-03 14:34:54 +0300

    CLASS UPDATE

--- index.php
+++ index.php
@@ -7,21 +7,33 @@ require 'kategoriagaci.class.php';
 <head>
 	<meta charset="UTF-8">
 	<link href="treeStyle.css" rel="stylesheet">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript">
+		$(function(){
+			$("input[name='category[]']").click(function () {
+				if($(this).is(':checked')){
+					$(this).parent().addClass("checked");
+				}
+				else{
+					$(this).parent().removeClass("checked");
+				}
+			});
+		});
+	</script>
 	<title>Kategori Ağacı</title>
 </head>
 <body>
 	<div class="tree-panel">
-		<div class="tree-panel-heading">
-			<a class="btn btn-default" href="#" id="hepsini-sec"><i class="fa fa-check-square"></i> Tümünü Seç</a>
-			<a class="btn btn-default" href="#" id="hepsini-cikar"><i class="fa fa-minus-square"></i> Tümünü Çıkar</a>
-		</div>
 		<ul id="home-tree" class="tree">
 			<?php
 			$array = array();
-			$array["ktg_id"][] = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
-			$array["ktg_adi"][] = array("Ana Kategori", "Elektronik", "Ev Aletleri", "Televizyon", "Bilgisayar", "Beyaz Eşya", "Buzdolabı", "Çamaşır Makinası", "Bulaşık Makinası");
-			$array["ktg_ust"][] = array(0, 1, 1, 2, 2, 3, 4, 4, 4);
-			KategoriAgaci::tree($array);
+			$array["ktg_id"] = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+			$array["ktg_adi"] = array("Ana Kategori", "Elektronik", "Ev Aletleri", "Televizyon", "Bilgisayar", "Beyaz Eşya", "Buzdolabı", "Çamaşır Makinası", "Bulaşık Makinası");
+			$array["ktg_ust"] = array(0, 1, 1, 2, 2, 3, 6, 6, 6);
+
+			$secilmisler = array(3, 7, 9);
+			echo KategoriAgaci::tree($array, null, $secilmisler);
 			?>
 		</ul>
 	</div>
--- kategoriagaci.class.php
+++ kategoriagaci.class.php
@@ -15,41 +15,42 @@ class KategoriAgaci{
      * @param array $selected
      * @return string
      */
-    static function tree($array, $inputName = "category", $ust_ktg = 0, $selected = array()){
+    static function tree($array, $ust_ktg = 0, $selected = array(), $inputName = "category"){
+        $dataArray = $array;
         $data = "";
         $data .= "<ul class='tree'>";
-        foreach($array["ktg_id"] as $index => $item){
-            if($array["ktg_ust"][$index] == $ust_ktg){
+        foreach($dataArray["ktg_id"] as $index => $item){
+            if($dataArray["ktg_ust"][$index] == $ust_ktg){
                 if(count($selected) != 0){
                     if(in_array($item, $selected)){
                         $data .= "<li class='tree-folder'>";
-                        $data .= "<span class='tree-folder-name'>";
-                        $data .= "<input checked type='checkbox' name='".$inputName."[]' value='".$array["ktg_id"][$index]."'>";
+                        $data .= "<span class='tree-folder-name checked'>";
+                        $data .= "<input checked type='checkbox' name='".$inputName."[]' value='".$dataArray["ktg_id"][$index]."'>";
                         $data .= "<i class='fa fa-folder-open'></i> ";
-                        $data .= "<label class='tree-toggle'>".$array['ktg_adi'][$index]."</label>";
+                        $data .= "<label class='tree-toggle'>".$dataArray['ktg_adi'][$index]."</label>";
                         $data .= "</span>";
-                        $data .= self::tree($array, $array["ktg_id"][$index], $selected);
+                        $data .= self::tree($dataArray, $dataArray["ktg_id"][$index], $selected);
                         $data .= "</li>";
                     }
                     else{
                         $data .= "<li class='tree-folder'>";
                         $data .= "<span class='tree-folder-name'>";
-                        $data .= "<input type='checkbox' name='".$inputName."[]' value='".$array["ktg_id"][$index]."'>";
+                        $data .= "<input type='checkbox' name='".$inputName."[]' value='".$dataArray["ktg_id"][$index]."'>";
                         $data .= "<i class='fa fa-folder-open'></i> ";
-                        $data .= "<label class='tree-toggle'>".$array['ktg_adi'][$index]."</label>";
+                        $data .= "<label class='tree-toggle'>".$dataArray['ktg_adi'][$index]."</label>";
                         $data .= "</span>";
-                        $data .= self::tree($array, $array["ktg_id"][$index], $selected);
+                        $data .= self::tree($dataArray, $dataArray["ktg_id"][$index], $selected);
                         $data .= "</li>";
                     }
                 }
                 else{
                     $data .= "<li class='tree-folder'>";
                     $data .= "<span class='tree-folder-name'>";
-                    $data .= "<input type='checkbox' name='".$inputName."[]' value='".$array["ktg_id"][$index]."'>";
+                    $data .= "<input type='checkbox' name='".$inputName."[]' value='".$dataArray["ktg_id"][$index]."'>";
                     $data .= "<i class='fa fa-folder-open'></i> ";
-                    $data .= "<label class='tree-toggle'>".$array['ktg_adi'][$index]."</label>";
+                    $data .= "<label class='tree-toggle'>".$dataArray['ktg_adi'][$index]."</label>";
                     $data .= "</span>";
-                    $data .= self::tree($array, $array["ktg_id"][$index]);
+                    $data .= self::tree($dataArray, $dataArray["ktg_id"][$index]);
                     $data .= "</li>";
                 }
             }
